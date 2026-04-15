8cb04024b1d2de4d1761dc400b42deea0aa6d821
Khaoula Benyahia <khaoulabenyahiaa@gmailcom>
2026-04-15 14:31:14 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react
 export default defineConfig({
+  base: "/Benyahia.github.io/",
   plugins: [react()],
-})
+});
